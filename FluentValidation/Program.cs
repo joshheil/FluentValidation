cc6175020e7b48ef5175725bce65d35ddd9f6ea2
joshheil <joshheil2@gmail.com>
2014-07-28 19:20:07 -0400

    Added MSpec and unit tests

--- FluentValidation/Program.cs
+++ FluentValidation/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using FluentValidation;
 using FluentValidation.Results;
+using FluentValidation.TestHelper;
+using Machine.Specifications;
 
 namespace FluentValidationPOC
 {
@@ -39,9 +41,57 @@ namespace FluentValidationPOC
 
         private void RequiredFieldsValidatorForNurse()
         {
-
             RuleFor(person => person.LastName).NotEmpty();
             RuleFor(person => person.FirstName).NotEmpty();
         }
+    }
+
+    public class when_validating_an_empty_person
+    {
+        private static PersonValidator personValidator;
+        private static ValidationResult validationResult;
+
+        private Establish context = () =>
+                                    {
+                                        personValidator = new PersonValidator();
+                                        validationResult = new ValidationResult();
+                                    };
+
+        Because of = () => validationResult = personValidator.Validate(new Person(), ruleSet: "RequiredFields");
+
+        private It should_fail_validation = () => validationResult.IsValid.ShouldEqual(false);
+    }
+
+
+    public class when_validating_a_person_populated_with_only_first_name
+    {
+        private static PersonValidator personValidator;
+        private static ValidationResult validationResult;
+
+        private Establish context = () =>
+        {
+            personValidator = new PersonValidator();
+            validationResult = new ValidationResult();
+        };
+
+        Because of = () => validationResult = personValidator.Validate(new Person(){DateofBirth = new DateTime(1928, 12,21), FirstName = "John", LastName = null}, ruleSet: "RequiredFields");
+
+        It should_still_fail_validation = () => validationResult.IsValid.ShouldEqual(false);
+    }
+
+    public class when_validating_a_populated_person
+    {
+        private static PersonValidator personValidator;
+        private static ValidationResult validationResult;
+
+        private Establish context = () =>
+        {
+            personValidator = new PersonValidator();
+            validationResult = new ValidationResult();
+        };
+
+        Because of = () => validationResult = personValidator.Validate(new Person() { DateofBirth = new DateTime(1928, 12, 21), FirstName = "John", LastName = "Doe" }, ruleSet: "RequiredFields");
+
+        It should_pass_validation = () => validationResult.IsValid.ShouldEqual(true);
     }
  }
